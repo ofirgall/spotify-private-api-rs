80e203310c2dc938ea6052c0d451e02bb39caf25
Ofir Gal <ofir@gal.software>
2022-10-11 17:46:20 +0300

    can create a folder

--- src/api/folders.rs
+++ src/api/folders.rs
@@ -1,11 +1,12 @@
 use std::collections::HashMap;
 
 use serde::{Deserialize, Serialize};
+use serde_json::json;
 use serde_json::Value;
 
 #[derive(Serialize, Deserialize, Debug)]
 pub struct RootList {
-    revision: String,
+    pub revision: String,
     length: u32,
 
     attributes: HashMap<String, Value>,
@@ -39,4 +40,128 @@ struct RootListMetaItem {
 
     #[serde(rename = "ownerUsername")]
     owner_username: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+pub struct Changes {
+    #[serde(rename = "baseRevision")]
+    base_revision: String,
+
+    #[serde(rename = "wantResultingRevisions")]
+    want_resulting_revisions: bool,
+
+    #[serde(rename = "wantSyncResult")]
+    want_sync_result: bool,
+
+    nonces: Vec<Value>,
+
+    deltas: Vec<Delta>,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct Delta {
+    ops: Vec<Operation>,
+    info: Value,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "kind")]
+enum Operation {
+    #[serde(rename = "ADD")]
+    Add(AddOperation),
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AddOperation {
+    #[serde(skip_serializing)]
+    kind: String,
+    add: AddOperationParams,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct AddOperationParams {
+    #[serde(rename = "fromIndex")]
+    from_index: u32,
+
+    items: Vec<OperationItem>,
+
+    #[serde(rename = "addLast")]
+    add_last: bool,
+
+    #[serde(rename = "addFirst")]
+    add_first: bool,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct OperationItem {
+    uri: String,
+    attributes: Value,
+}
+
+// TODO: tmp function to test structs
+pub fn add_folder(revision: &str, name: &str, start_index: u32, end_index: u32) -> Changes {
+    let op_item_attrs = json!({
+        "addedBy": "",
+        "timestamp": "1665585275953",
+        "seenAt": "0",
+        "public": false,
+        "formatAttributes": []
+    });
+    Changes {
+        base_revision: revision.to_string(),
+        want_resulting_revisions: false,
+        want_sync_result: false,
+        nonces: vec![],
+        deltas: vec![Delta {
+            ops: vec![
+                Operation::Add(AddOperation {
+                    kind: "ADD".to_string(),
+                    add: AddOperationParams {
+                        from_index: start_index,
+                        items: vec![
+                            OperationItem {
+                                uri: format!("spotify:start-group:123456789abcdefa:{}", name),
+                                attributes: op_item_attrs.clone(),
+                            },
+                            OperationItem {
+                                uri: "spotify:end-group:123456789abcdefa".to_string(),
+                                attributes: op_item_attrs,
+                            },
+                        ],
+                        add_last: false,
+                        add_first: false,
+                    },
+                }),
+                // Operation::Add(AddOperation {
+                //     kind: "ADD".to_string(),
+                //     add: AddOperationParams {
+                //         from_index: end_index,
+                //         items: vec![OperationItem {
+                //             uri: format!("spotify:end-group:123456789abcdefa:{}", name),
+                //             attributes: op_item_attrs,
+                //         }],
+                //         add_last: false,
+                //         add_first: false,
+                //     },
+                // }),
+            ],
+            info: json!({
+                "user": "",
+                "timestamp": "0",
+                "admin": false,
+                "undo": false,
+                "redo": false,
+                "merge": false,
+                "compressed": false,
+                "migration": false,
+                "splitId": 0i32,
+                "source": {
+                    "client": "WEBPLAYER",
+                    "app": "",
+                    "source": "",
+                    "version": ""
+                }
+            }),
+        }],
+    }
 }
--- src/session.rs
+++ src/session.rs
@@ -42,6 +42,42 @@ impl Session {
 
         Ok(serde_json::from_str(&res)?)
     }
+
+    pub async fn add_folder(
+        &self,
+        revision: &str,
+        name: &str,
+        start_index: u32,
+        end_index: u32,
+    ) -> Result<()> {
+        self.send_changes(&api::folders::add_folder(
+            revision,
+            name,
+            start_index,
+            end_index,
+        ))
+        .await
+    }
+
+    async fn send_changes(&self, changes: &api::folders::Changes) -> Result<()> {
+        println!("{}", serde_json::to_string_pretty(changes)?);
+        self.http_client
+            .post(format!(
+                "https://spclient.wg.spotify.com/playlist/v2/user/{}/rootlist/changes",
+                self.user_id
+            ))
+            .header("Accept", "application/json")
+            .header("app-platform", "WebPlayer")
+            .header("authorization", format!("Bearer {}", self.access_token))
+            .header("client-token", &self.client_token)
+            .header("content-type", "application/json;charset=UTF-8") // TODO: parse names in this charsets
+            .body(serde_json::to_string(changes)?)
+            .send()
+            .await?
+            .error_for_status()?;
+
+        Ok(())
+    }
 }
 
 #[cfg(test)]
@@ -58,5 +94,18 @@ mod tests {
         println!("{:?}", s);
         let root_list = s.get_root_list().await.unwrap();
         println!("{:?}", root_list);
+    }
+
+    #[tokio::test]
+    async fn test_create_folder() {
+        let dc = std::env::var("SPOTIFY_DC").unwrap();
+        let key = std::env::var("SPOTIFY_KEY").unwrap();
+        let user_id = std::env::var("SPOTIFY_USER_ID").unwrap();
+
+        let s = Session::new(&dc, &key, &user_id).await.unwrap();
+        let root_list = s.get_root_list().await.unwrap();
+        s.add_folder(&root_list.revision, "Abablagan", 0, 2)
+            .await
+            .unwrap();
     }
 }
