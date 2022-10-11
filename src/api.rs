76505f8321cd9ba29550a74cc0f6f62beadc9610
Ofir Gal <ofir@gal.software>
2022-10-11 13:28:51 +0300

    First private api successful request

---
+++ src/api.rs
@@ -0,0 +1,115 @@
+use std::{collections::HashMap, time::SystemTime};
+
+use serde::{Deserialize, Serialize};
+
+use serde_with::formats::Flexible;
+use serde_with::TimestampMilliSeconds;
+
+use serde_json::{Result, Value};
+
+#[serde_with::serde_as]
+#[derive(Serialize, Deserialize, Debug)]
+pub struct AccessTokenResponse {
+    #[serde(rename = "accessToken")]
+    pub access_token: String,
+
+    #[serde(rename = "clientId")]
+    pub client_id: String,
+
+    #[serde_as(as = "TimestampMilliSeconds<String, Flexible>")]
+    #[serde(rename = "accessTokenExpirationTimestampMs")]
+    expiration_time: SystemTime,
+
+    #[serde(rename = "isAnonymous")]
+    is_anonymous: bool,
+}
+
+const FAKE_USER_AGENT: &str = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/105.0.0.0 Safari/537.36";
+
+pub async fn get_access_token(
+    http_client: &reqwest::Client,
+    dc: &str,
+    key: &str,
+) -> Result<AccessTokenResponse> {
+    let res = http_client
+        .get("https://open.spotify.com/get_access_token?reason=transport&productType=web_player")
+        .header("user-agent", FAKE_USER_AGENT)
+        .header("Cookie", format!("sp_dc={};sp_key={}", dc, key))
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    serde_json::from_str(&res)
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientTokenRequest {
+    client_data: ClientTokenRequestData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientTokenRequestJsSdkData {
+    device_brand: String,
+    device_model: String,
+    os: String,
+    os_version: String,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientTokenRequestData {
+    client_id: String,
+    client_version: String,
+    js_sdk_data: ClientTokenRequestJsSdkData,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientTokenResponse {
+    response_type: String,
+    granted_token: ClientTokenResponseGranted,
+}
+
+#[derive(Serialize, Deserialize, Debug)]
+struct ClientTokenResponseGranted {
+    token: String,
+    expires_after_seconds: u32,
+    refresh_after_seconds: u32,
+
+    #[serde(flatten)]
+    extra: HashMap<String, Value>,
+}
+
+pub async fn get_client_token(http_client: &reqwest::Client, client_id: &str) -> Result<String> {
+    let fake_client_token_request = ClientTokenRequest {
+        client_data: ClientTokenRequestData {
+            client_id: client_id.to_string(),
+            client_version: "1.1.97.136.g81a082e9".to_string(),
+            js_sdk_data: ClientTokenRequestJsSdkData {
+                device_brand: "unknown".to_string(),
+                device_model: "desktop".to_string(),
+                os: "Linux".to_string(),
+                os_version: "unknown".to_string(),
+            },
+        },
+    };
+
+    let json = serde_json::to_string(&fake_client_token_request)?;
+
+    let res_body = http_client
+        .post("https://clienttoken.spotify.com/v1/clienttoken")
+        .header("Accept", "application/json")
+        .header("Content-Type", "application/json")
+        .body(json)
+        .send()
+        .await
+        .unwrap()
+        .text()
+        .await
+        .unwrap();
+
+    let res: ClientTokenResponse = serde_json::from_str(&res_body)?;
+
+    Ok(res.granted_token.token)
+}
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,58 @@
+mod api;
+use serde_json::Result;
+
+#[derive(Debug)]
+pub struct Session {
+    user_id: String,
+
+    http_client: reqwest::Client,
+
+    access_token: String,
+    client_id: String,
+    client_token: String,
+}
+
+impl Session {
+    pub async fn new(dc: &str, key: &str, user_id: &str) -> Result<Self> {
+        let http_client = reqwest::Client::new();
+        let access_token_resp = api::get_access_token(&http_client, dc, key).await?;
+        let client_token =
+            api::get_client_token(&http_client, &access_token_resp.client_id).await?;
+
+        Ok(Self {
+            user_id: user_id.to_string(),
+            http_client,
+            access_token: access_token_resp.access_token,
+            client_id: access_token_resp.client_id,
+            client_token,
+        })
+    }
+
+    pub async fn get_root_list(&self) {
+        let res = self.http_client
+            .get(format!("https://spclient.wg.spotify.com/playlist/v2/user/{}/rootlist?decorate=revision%2Clength%2Cattributes%2Ctimestamp%2Cowner", self.user_id))
+            .header("Accept", "application/json")
+            .header("app-platform", "WebPlayer")
+            .header("authorization", format!("Bearer {}", self.access_token))
+            .header("client-token", &self.client_token)
+            .send()
+            .await.unwrap().text().await.unwrap();
+        println!("Res: {}", res);
+    }
+}
+
 #[cfg(test)]
 mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+    use crate::Session;
+
+    #[tokio::test]
+    async fn test_new_session() {
+        let dc = std::env::var("SPOTIFY_DC").unwrap();
+        let key = std::env::var("SPOTIFY_KEY").unwrap();
+        let user_id = std::env::var("SPOTIFY_USER_ID").unwrap();
+
+        let s = Session::new(&dc, &key, &user_id).await.unwrap();
+        println!("{:?}", s);
+        s.get_root_list().await;
     }
 }
